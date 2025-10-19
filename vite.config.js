7d9ca2a381d91fb71f35fb778c6ef5775b406991
farrukh850 <farrukhwaseem850@gmail.com>
2025-10-20 02:14:38 +0500

    Pushing first milestone pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'step-1': resolve(__dirname, 'authentication/step-1.html'),
+        'step-2': resolve(__dirname, 'authentication/step-2.html'),
+        'step-3': resolve(__dirname, 'authentication/step-3.html'),
+        'collection-posts': resolve(__dirname, 'collections/collection-posts.html'),
+        'collection-posts-empty': resolve(__dirname, 'collections/collection-posts-empty.html'),
+        'create-new-post': resolve(__dirname, 'create-new-post/create-new-post.html'),
+        'messages': resolve(__dirname, 'messages/messages.html'),
+        'notifications': resolve(__dirname, 'notifications/notifications.html')
+      }
+    }
+  }
 })
